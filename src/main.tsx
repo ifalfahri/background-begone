1a90eb70ae393f94a64540441ed6b8c17d9f6b4e
ifalfahri <ifaltampan69@gmail.com>
2025-01-29 11:10:39 +0800

    refactor: implement BackgroundRemover component for background removal functionality

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import BackgroundRemover from './BackgroundRemover.tsx'
+import BackgroundRemover from '@/components/BackgroundRemover'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
